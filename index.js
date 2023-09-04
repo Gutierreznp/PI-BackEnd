15bc57fb15dbedda6b1f8f41394060f7ef01b166
Gutierreznp <gutierreznp@gmail.com>
2023-09-04 13:11:01 -0300

    PI todo el back

---
+++ index.js
@@ -0,0 +1,27 @@
+const axios = require("axios");
+const server = require("./src/server");
+const { conn } = require('./src/db.js');
+const PORT = 3001;
+const {Country} = require('./src/db');
+
+conn.sync({ force: true }).then(() => {
+server.listen(PORT, async () => {
+  const allCountries = await Country.findAll();
+  if (!allCountries.length) {
+    const response = await axios(`http://localhost:5000/countries`);
+    let countriesDB = response.data.map((country) => {
+      return {
+        id: country.cca3,
+        name: country.name.common,
+        flag: country.flags.png,
+        region: country.region,
+        capital: country.capital ? country.capital[0] : 'No tiene capital!',
+        subregion: country.subregion,
+        population: country.population
+      }
+    })
+    await Country.bulkCreate(countriesDB);
+  }
+  console.log(`Server running on port ${PORT}`);
+})
+}).catch(error => console.error(error))
